b8061360f08d92c90734b8cc2e489cdb1f50eb22
LalitLab <lt.b1991@gmail.com>
2023-01-05 00:24:52 +0530

    Add end to end test to pipeline workflow

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,18 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:3000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Test squirtle page', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('squirtle').click()
+    cy.contains('torrent')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
 })
+
+// To run test locally on the system run below steps in order
+// 1. npm run build && node app.js
+// 2. In a separate tab run "npm run cypress:open"
